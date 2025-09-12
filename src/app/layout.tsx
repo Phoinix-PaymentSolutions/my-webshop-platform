5e9e5e00646b050fc6dde164ef1c2d06acd1a395
Phoinix-PaymentSolutions <contact@phoinix.nl>
2025-09-12 14:41:36 +0200

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { GeistSans } from 'geist/font/sans';
+import { GeistMono } from 'geist/font/mono';
 import "./globals.css";
 
 const geistSans = Geist({
