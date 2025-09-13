f392994eb815c1760300ced6877033f514ef3d95
Phoinix-PaymentSolutions <contact@phoinix.nl>
2025-09-13 23:52:35 +0200

    FIXED COMMIT MESSAGE

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,7 @@ module.exports = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     extend: {},
